234f17e86783bdc8317403e9352ccf77d0cc4f3e
saze1997 <saze1997@gmail.com>
2016-01-24 14:41:51 +0200

    gitignore updated & models

--- server/serverdict_db/terms/models.py
+++ server/serverdict_db/terms/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=128)
+
+class TermHistory(models.Model):
+    author = models.ForeignKey(Author)
+    year = models.IntegerField(blank=True, null=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+class Term(models.Model):
+    name = models.CharField(max_length=128)
+    definition = models.TextField()
+    category = models.ForeignKey(Category)
+    history = models.ForeignKey(TermHistory, )
