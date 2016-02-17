edd20c99fe75f8088188402838d677823648227a
Oleksandr Zelentsov <saze1997@gmail.com>
2016-02-17 13:33:25 +0200

    tests
     * renamed the test to represent Term test
     * wrote test for access and popularity

--- server/serverdict_db/terms/tests.py
+++ server/serverdict_db/terms/tests.py
@@ -3,8 +3,9 @@ from terms.models import *
 # Create your tests here.
 
 
-class TermsAccessibilityTest(TestCase):
+class TermTest(TestCase):
     def setUp(self):
+        # create category
         Category.objects.create(name='cat1')
         # create users
         user1 = User.objects.create_user('john', 'john@thebeatles.com', 'johnpassword')
@@ -22,10 +23,20 @@ class TermsAccessibilityTest(TestCase):
         term1.grant_access(user1, user2)
         term2.grant_access(user3)
 
-    def test_accessibility(self):
-        self.assertGreater(Term.objects.get(name='term #1').popularity, Term.objects.get(name='term #2').popularity)
+    def test_popularity_is_affected(self):
+        """The popularity is affeced granting an access."""
+        self.assertGreater(
+        Term.objects.get(name='term #1').popularity,
+        Term.objects.get(name='term #2').popularity)
+
+    def test_users_have_access(self):
+        """Users have access to terms."""
+        self.assertIn(User.objects.get(username='john'), Term.objects.get(name='term #1').accesibility.all())
+        self.assertIn(User.objects.get(username='paul'), Term.objects.get(name='term #1').accesibility.all())
+        self.assertIn(User.objects.get(username='george'), Term.objects.get(name='term #2').accesibility.all())
 
     def tearDown(self):
+        # deny access to terms for users
         Term.objects.get(name='term #2').forbid_access(User.objects.get(username='george'))
         Term.objects.get(name='term #1').forbid_access(User.objects.get(username='john'), User.objects.get(username='paul'))
         # delete terms
@@ -36,4 +47,5 @@ class TermsAccessibilityTest(TestCase):
         User.objects.get(username='george').delete()
         User.objects.get(username='paul').delete()
         User.objects.get(username='john').delete()
+        # delete category
         Category.objects.get(name='cat1').delete()
