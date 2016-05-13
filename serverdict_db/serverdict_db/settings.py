d3a6c596a892d67e1d4e4cde7b1cbea4cc1bce16
Oleksandr Zelentsov <saze1997@gmail.com>
2016-05-13 17:38:35 +0300

    prod settings file

--- serverdict_db/serverdict_db/settings.py
+++ serverdict_db/serverdict_db/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cj7jcin%41cy-jvt-nb(ipv_zoik-zo4@wdv_3_i5l(paeifqt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
